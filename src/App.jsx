638f1e7d593d4e883c6a3f22da6d2aae8811835f
j0srisk <j0srisk@gmail.com>
2023-05-28 19:57:21 -0500

    added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NewTodoForm } from "./NewTodoForm"
 import "./styles.css"
 import { TodoList } from "./TodoList"
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
     setTodos(currentTodos => {
@@ -37,7 +46,11 @@ export default function App() {
     <>
       <NewTodoForm onSubmit= {addTodo}/>
       <h1 className="header">Todo List</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      <TodoList 
+        todos={todos} 
+        toggleTodo={toggleTodo} 
+        deleteTodo={deleteTodo}
+      />
     </>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,17 +3,17 @@ import { TodoItem } from "./TodoItem";
 export function TodoList({ todos, toggleTodo, deleteTodo}) {
     return (
         <ul className="list">
-        {todos.length === 0 && "No todos yet!"}
-        {todos.map(todo => {
-          return (
-            <TodoItem 
-                {...todo} 
-                key={todo.id} 
-                toggleTodo={toggleTodo}
-                deleteTodo={deleteTodo}
-            />
-          )
-        })}
-      </ul>
+            {todos.length === 0 && "No todos yet!"}
+            {todos.map(todo => {
+            return (
+                <TodoItem 
+                    {...todo} 
+                    key={todo.id} 
+                    toggleTodo={toggleTodo}
+                    deleteTodo={deleteTodo}
+                />
+            )
+            })}
+        </ul>
     )
 }
